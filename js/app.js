13434ab8b3ca349e40f5eed872a43e6254ee91af
squteronsquter <squter@squter.pl>
2019-03-20 19:44:01 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// $('.footer-nav-menu-item').on('click', function() {
+//   var label = $(this).attr('data-label');
+//   $('.panel').removeClass('active');
+//   $('#' + label).addClass('active');
+// });
+
+$('.footer-nav-menu-item').on('click', function() {
+  var label = $(this).attr('data-label');
+  $('.panel-header').text(label);
+  $('.panel').addClass('active');
+});
+
+$('.panel-close').on('click', function() {
+  $('.panel').removeClass('active');
+});
+$('a').on('click', function() {
+  $('.panel').removeClass('active');
+});
